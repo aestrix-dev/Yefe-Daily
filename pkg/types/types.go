4592ba590bc0bafac84b9f268d27927872e3663d
emperorsixpacks <andrewdvd10@gmail.com>
2025-06-21 10:00:53 +0100

    feat: added password config

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -3,3 +3,12 @@ package types
 type HttpHander interface {
 	Handle()
 }
+
+// Password security configuration
+type PasswordConfig struct {
+	Memory      uint32
+	Iterations  uint32
+	Parallelism uint8
+	SaltLength  uint32
+	KeyLength   uint32
+}
