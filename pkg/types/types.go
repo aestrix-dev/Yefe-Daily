dc031219508bada00ae48f7a2bf47b2457680150
emperorsixpacks <andrewdvd10@gmail.com>
2025-06-21 10:36:30 +0100

    feat: security events

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -12,3 +12,25 @@ type PasswordConfig struct {
 	SaltLength  uint32
 	KeyLength   uint32
 }
+
+type SecurityEventType string
+
+const (
+	EventLogin              SecurityEventType = "login"
+	EventLoginFailed        SecurityEventType = "login_failed"
+	EventLogout             SecurityEventType = "logout"
+	EventPasswordChange     SecurityEventType = "password_change"
+	EventPasswordReset      SecurityEventType = "password_reset"
+	EventAccountLocked      SecurityEventType = "account_locked"
+	EventAccountUnlocked    SecurityEventType = "account_unlocked"
+	EventSuspiciousActivity SecurityEventType = "suspicious_activity"
+)
+
+type EventSeverity string
+
+const (
+	SeverityInfo     EventSeverity = "info"
+	SeverityWarning  EventSeverity = "warning"
+	SeverityError    EventSeverity = "error"
+	SeverityCritical EventSeverity = "critical"
+)
